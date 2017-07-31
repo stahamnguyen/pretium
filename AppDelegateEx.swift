980d5f33dbc5259c21817d49cfe42753f5efb1af
stahamnguyen <nguyensnj@gmail.com>
2017-07-31 20:15:10 +0300

    Added initial files

---
+++ AppDelegateEx.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension AppDelegate {
+    class func isIPhone5 () -> Bool{
+        return max(UIScreen.main.bounds.width, UIScreen.main.bounds.height) == 568.0
+    }
+    class func isIPhone6 () -> Bool {
+        return max(UIScreen.main.bounds.width, UIScreen.main.bounds.height) == 667.0
+    }
+    class func isIPhone6Plus () -> Bool {
+        return max(UIScreen.main.bounds.width, UIScreen.main.bounds.height) == 736.0
+    }
+}
---
+++ Pretium/CustomButton.swift
@@ -0,0 +1,22 @@
+//
+//  CustomButton.swift
+//  Pretium
+//
+//  Created by Staham Nguyen on 30/07/2017.
+//  Copyright © 2017 Staham Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+
+    init() {
+        super.init(frame: CGRect.zero)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+}
